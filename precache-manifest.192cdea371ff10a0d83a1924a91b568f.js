4a5838762969308358ab1b3acf0156313a4a66e0
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-12 16:09:19 +0200

    Updates

---
+++ precache-manifest.192cdea371ff10a0d83a1924a91b568f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4650862c8d2513ef6950dd4186d146",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/index.html"
+  },
+  {
+    "revision": "410b256aa439bc55acc2",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/css/main.2097d407.chunk.css"
+  },
+  {
+    "revision": "ad1e5d0b6f7a121830dd",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/js/2.c460eb68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/js/2.c460eb68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410b256aa439bc55acc2",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/js/main.44b9425c.chunk.js"
+  },
+  {
+    "revision": "2d762e057d74e2d5abf8",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/js/runtime-main.44975eba.js"
+  },
+  {
+    "revision": "8c0d54eec3b176acc6c2b563b7aad76c",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/media/budget-project.8c0d54ee.jpg"
+  },
+  {
+    "revision": "2e1f009fe4b2bba69bd0d77cb670c42d",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/media/burger-project.2e1f009f.jpg"
+  },
+  {
+    "revision": "7311356c97112038ac20f2cce99b26b0",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/media/obaju-project.7311356c.jpg"
+  },
+  {
+    "revision": "f1fbf21fc8822b89c4151bd46c36269d",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/media/overlay-bg.f1fbf21f.jpg"
+  },
+  {
+    "revision": "45038293d908d903b418cf542fd879e5",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/media/profile.45038293.jpg"
+  },
+  {
+    "revision": "0bcf8deefa707a5f48b14179bdbafd8b",
+    "url": "/http//:DuncanMoyo.github.io/portfolio-react/static/media/table.0bcf8dee.jpg"
+  }
+]);
